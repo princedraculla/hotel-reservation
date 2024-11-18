37e8c16e4f48f652e76c55771b5b27bfb169e100
princedraculla <amirtorkashvand56@gmail.com>
2024-11-18 18:09:41 +0330

    module tidy

--- types/user.go
+++ types/user.go
@@ -3,5 +3,5 @@ package types
 type User struct {
 	ID        int    `bson:"_id" json:"id,omitempty"`
 	FirstName string `bson:"first_name" json:"firstName"`
-	LastName  string `bson:"last_name json:"lastName`
+	LastName  string `bson:"last_name" json:"lastName"`
 }
