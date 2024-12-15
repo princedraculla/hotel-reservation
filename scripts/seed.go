c31423754f4383b50a9b589046b5130a318bced5
princedraculla <amirtorkashvand56@gmail.com>
2024-12-15 19:47:28 +0330

    (db): client & collection hotel

--- scripts/seed.go
+++ scripts/seed.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"context"
+	"fmt"
+
+	"github.com/princedraculla/hotel-reservation/db"
+	"github.com/princedraculla/hotel-reservation/types"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
 func main() {
 
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(db.DBURI).SetServerAPIOptions(serverAPI)
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(context.TODO(), opts)
+	if err != nil {
+		panic(err)
+	}
+
+	hotelStore := db.NewMongoHotelStore(client, db.DBNAME)
+
+	hotel := types.Hotel{
+		Name:     "Taj Mahal Hotel",
+		Location: "Tehran,iran",
+	}
+	rooms := types.Room{
+		Type:      types.DeluxeRoomType,
+		BasePrice: 99.9,
+	}
+
+	fmt.Println("seeding Database...!")
 }
