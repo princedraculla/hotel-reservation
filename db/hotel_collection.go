dc32d4e17c04df4e6db9aea37890c351f83845b5
princedraculla <amirtorkashvand56@gmail.com>
2024-12-17 19:41:45 +0330

    (database): update hotel with room id

--- db/hotel_collection.go
+++ db/hotel_collection.go
@@ -4,12 +4,14 @@ import (
 	"context"
 
 	"github.com/princedraculla/hotel-reservation/types"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type HotelStore interface {
 	InsertHotel(context.Context, *types.Hotel) (*types.Hotel, error)
+	Update(context.Context, bson.M, bson.M) error
 }
 
 type MongoHotelStore struct {
@@ -31,4 +33,12 @@ func (hotelStore *MongoHotelStore) InsertHotel(ctx context.Context, hotel *types
 	}
 	hotel.ID = insertedHotel.InsertedID.(primitive.ObjectID)
 	return hotel, nil
+}
+
+func (hotelStore *MongoHotelStore) Update(ctx context.Context, filter bson.M, update bson.M) error {
+	_, err := hotelStore.collection.UpdateOne(ctx, filter, update)
+	if err != nil {
+		return err
+	}
+	return nil
 }
