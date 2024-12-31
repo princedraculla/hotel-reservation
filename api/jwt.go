a9d19491dcdb9604fb25430cc29a433878df0d04
princedraculla <amirtorkashvand56@gmail.com>
2024-12-31 19:53:06 +0330

    (Auth): user auth

--- api/jwt.go
+++ api/jwt.go
@@ -3,81 +3,59 @@ package api
 import (
 	"fmt"
 	"net/http"
-	"os"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/princedraculla/hotel-reservation/db"
-	"github.com/princedraculla/hotel-reservation/types"
 )
 
-var (
-	secretKey = os.Getenv("secret_key")
-)
+func JWTAuthenticate(userStore db.UserStore) fiber.Handler {
+	return func(ctx *fiber.Ctx) error {
+		token := ctx.Get("Authorization")
+		if token == "" {
+			fmt.Println("token not found")
+			return ErrUnAuthorized()
+		}
+		claims, err := ValidateToken(token)
+		if err != nil {
+			return err
+		}
+		// converting time.duration to float type to can conver to int
+		expires := int64(claims["exp"].(float64))
+		if time.Now().Unix() > expires {
+			return NewError(http.StatusUnauthorized, "token expired & sign in first")
+		}
+		userID := claims["id"].(string)
+		user, err := userStore.GetUserByID(ctx.Context(), userID)
+		if err != nil {
+			return ErrUnAuthorized()
+		}
+		ctx.Context().SetUserValue("user", user)
+		return ctx.Next()
 
-type AuthHandler struct {
-	userStore db.UserStore
-}
-
-func NewAuthHandler(userStore db.UserStore) *AuthHandler {
-	return &AuthHandler{
-		userStore: userStore,
 	}
 }
 
-type AuthParams struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
-
-type AuthResponse struct {
-	User  *types.User `json:"user"`
-	Token string      `json:"token"`
-}
-
-type genericResp struct {
-	Type string `json:"type"`
-	Msg  string `json:"msg"`
-}
-
-func InvalidCredentials(ctx *fiber.Ctx) error {
-	return ctx.Status(http.StatusBadRequest).JSON(genericResp{
-		Type: "error",
-		Msg:  "invalid credentials",
+func ValidateToken(tokenStr string) (jwt.MapClaims, error) {
+	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			fmt.Println("invalid signing method", token.Header["alg"])
+			return nil, ErrUnAuthorized()
+		}
+		return []byte(secretKey), nil
 	})
-}
-
-func (ah *AuthHandler) HandleAutheticate(ctx *fiber.Ctx) error {
-	var params AuthParams
-	if err := ctx.BodyParser(&params); err != nil {
-		return err
-	}
-	user, err := ah.userStore.GetUserByEmail(ctx.Context(), params.Email)
 	if err != nil {
-		return err
+		fmt.Println("faild to parse token", err)
+		return nil, ErrUnAuthorized()
 	}
-	if !types.IsValidPassword(user.EncryptedPassword, params.Password) {
-		return InvalidCredentials(ctx)
+	if !token.Valid {
+		fmt.Println("invalid token")
+		return nil, ErrUnAuthorized()
 	}
-	resp := AuthResponse{
-		User:  user,
-		Token: CreateToken(user),
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return nil, ErrUnAuthorized()
 	}
-	return ctx.JSON(resp)
-}
-
-func CreateToken(user *types.User) string {
-
-	claims := jwt.MapClaims{
-		"id":    user.ID,
-		"email": user.Email,
-		"exp":   time.Now().Add(time.Hour * 2).Unix(),
-	}
-	toekn := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	tokenStr, err := toekn.SignedString([]byte(secretKey))
-	if err != nil {
-		fmt.Println("faild to sign token with secret", err)
-	}
-	return tokenStr
+	return claims, nil
 }
