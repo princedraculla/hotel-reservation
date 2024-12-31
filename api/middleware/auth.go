a28367787e3feecd583c55db6a611038b3b97566
princedraculla <amirtorkashvand56@gmail.com>
2024-12-31 12:40:30 +0330

    (Authenticate): verifying token function

--- api/middleware/auth.go
+++ api/middleware/auth.go
@@ -1,10 +1,15 @@
 package middleware
 
 import (
+	"fmt"
 	"os"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
+)
+
+var (
+	secretKey = os.Getenv("secret_key")
 )
 
 func JWTAuthentication() {
@@ -17,9 +22,22 @@ func CreateToken(email string, password string) (string, error) {
 		"password": password,
 		"exp":      time.Now().Add(time.Hour * 24).Unix(),
 	})
-	tokenStr, err := token.SignedString(os.Getenv("secret_key"))
+	tokenStr, err := token.SignedString(secretKey)
 	if err != nil {
 		return "", nil
 	}
 	return tokenStr, nil
+}
+
+func VerifyToken(tokenStr string) error {
+	token, err := jwt.Parse(tokenStr, func(t *jwt.Token) (interface{}, error) {
+		return secretKey, nil
+	})
+	if err != nil {
+		return err
+	}
+	if !token.Valid {
+		return fmt.Errorf("toekn its not value %s", token)
+	}
+	return nil
 }
